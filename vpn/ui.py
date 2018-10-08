cde32467f8aa5b1e360973560ea93767da7b82bb
Joy <d4f0b@ugrad.cs.ubc.ca>
2018-10-07 17:03:27 -0700

    textbox created

--- vpn/ui.py
+++ vpn/ui.py
@@ -29,6 +29,11 @@ ttk.Label(mainframe, text="Shared Secret").grid(column=1, row=2, sticky=W)
 ttk.Label(mainframe, text="Data to Send").grid(column=1, row=3, sticky=W)
 ttk.Label(mainframe, text="Data Received").grid(column=1, row=4, sticky=W)
 
+# text box
+textbox = Text(mainframe, borderwidth=3, width = 30, relief="sunken")
+textbox.grid(column=2, row=5, sticky=(W, E))
+
+
 toggleVal = IntVar()
 clientToggle = ttk.Radiobutton(mainframe, text="Client", variable=toggleVal, value=1)
 serverToggle = ttk.Radiobutton(mainframe, text="Server", variable=toggleVal, value=2)
@@ -37,7 +42,7 @@ clientToggle.grid(column=3, row=1, sticky=W)
 serverToggle.grid(column=4, row=1, sticky=W)
 # buttons
 
-ttk.Button(mainframe, text="Continue").grid(column=2, row=5, sticky=W)
+ttk.Button(mainframe, text="Continue").grid(column=3, row=5, sticky=W)
 
 ttk.Button(mainframe, text="Send").grid(column=3, row=3, sticky=W)
 
